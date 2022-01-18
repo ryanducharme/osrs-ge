b1d97ab89d4692b7cdbfdeee41fb16d4ce36c3d5
Ryan <72882588+ryanducharme@users.noreply.github.com>
2022-01-18 01:15:44 -0500

    update

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ $(function () {
     var itemMapping;
     var itemPriceData;
     let itemNames = [];
+    //https://runescape.wiki/w/Application_programming_interface
 
+
+
+    $('#5m-radio-button').click(function(){
+        $('5m-radio-button').prop('checked', true);
+    });
+    $('#1h-radio-button').click(function(){
+        $('#1h-radio-button').toggle(this.checked);
+    });
+    $('#24h-radio-button').click(function(){
+        $('#24h-radio-button').toggle(this.checked);
+    });
     //Get item mapping data
     $.ajax({
         type: 'GET',
@@ -27,16 +39,44 @@ $(function () {
         }
     });
 
-    // $('5m-radio-button').click(function(){
-    //     $('5m-radio-button').prop('checked', true)
-    // });
+    
 
     $('#SearchButton').click(function () {
         var searchFor = $('#search-input').val();
-        if ($("5m-radio-button").attr("checked", true)) {
+        if ($("#5m-radio-button").is(':checked')) {
             get5MPrices()
                 .done(function (data) {
                     console.log('success (5m prices)', data);
+                    itemPriceData = data;
+                    weGotPrices();
+                })
+                .fail(function () {
+                    // oh shit
+                    window.alert('error');
+                })
+                .always(function () {
+                    // console.log()
+                });
+        }
+        if ($("#1h-radio-button").is(':checked')) {
+            get1HPrices()
+                .done(function (data) {
+                    console.log('success (1h prices)', data);
+                    itemPriceData = data;
+                    weGotPrices();
+                })
+                .fail(function () {
+                    // oh shit
+                    window.alert('error');
+                })
+                .always(function () {
+                    // console.log()
+                });
+        }
+        if ($("#24h-radio-button").is(':checked')) {
+            get24HPrices()
+                .done(function (data) {
+                    console.log('success 24H prices)', data);
                     itemPriceData = data;
                     weGotPrices();
                 })
@@ -55,8 +95,8 @@ $(function () {
             //find item in itemPriceData by its ID
             //compare and presenet the price data if both id's match
             var wantedItem = itemMapping.find(o => o.name.toLowerCase() === searchFor.toLowerCase())
-            $('#item-name').text('Item: ' + wantedItem.name + ' ID:' + wantedItem.id);
-
+            $('#item-name').text('Item: ' + wantedItem.name);
+            $('#item-id').text('ID: ' + wantedItem.id);
             var keys = Object.keys(itemPriceData.data);
 
             for (var i = 0; i < keys.length; i++) {
@@ -70,7 +110,9 @@ $(function () {
                     var margin = avgHighPrice - avgLowPrice;
                     var profit = margin * wantedItem.limit;
                     console.log(wantedItem.id);
-                    $('#item-image').attr('src', `https://secure.runescape.com/m=itemdb_oldschool/1641812469448_obj_big.gif?id=${wantedItem.id}`);
+                    // new * https://secure.runescape.com/m=itemdb_oldschool/1642417367576_obj_big.gif?id=12934
+                    $('#item-image').attr('src', `https://services.runescape.com/m=itemdb_oldschool/obj_big.gif?id=${wantedItem.id}`);
+                    
                     $('#buy-price').text('Buy: ' + new Intl.NumberFormat().format(avgHighPrice));
                     $('#sell-price').text('Sell: ' + new Intl.NumberFormat().format(avgLowPrice));
                     $('#margin').text('Margin: ' + new Intl.NumberFormat().format(margin));
@@ -102,26 +144,34 @@ $(function () {
 
     //Get 1h price data
     function get1HPrices() {
-        $.ajax({
+        // $.ajax({
+        //     type: 'GET',
+        //     url: 'https://prices.runescape.wiki/api/v1/osrs/1h',
+        //     success: function (data) {
+        //         console.log('success (5m prices)', data);
+        //         itemPriceData = data;
+        //     }
+        // });
+        return $.ajax({
             type: 'GET',
-            url: 'https://prices.runescape.wiki/api/v1/osrs/1h',
-            success: function (data) {
-                console.log('success (5m prices)', data);
-                itemPriceData = data;
-            }
+            url: 'https://prices.runescape.wiki/api/v1/osrs/1h'
         });
     }
 
 
     //Get 24h price data
     function get24HPrices() {
-        $.ajax({
+        // $.ajax({
+        //     type: 'GET',
+        //     url: 'https://prices.runescape.wiki/api/v1/osrs/24h',
+        //     success: function (data) {
+        //         console.log('success (5m prices)', data);
+        //         itemPriceData = data;
+        //     }
+        // });
+        return $.ajax({
             type: 'GET',
-            url: 'https://prices.runescape.wiki/api/v1/osrs/24h',
-            success: function (data) {
-                console.log('success (5m prices)', data);
-                itemPriceData = data;
-            }
+            url: 'https://prices.runescape.wiki/api/v1/osrs/24h'
         });
     }
 
